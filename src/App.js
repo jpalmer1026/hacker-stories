1c655d1d24a46dedae980a91b2f460c1d7f91f7e
Jeff Palmer <palmer1026@gmail.com>
2021-05-02 08:15:37 -0500

    Destructure component props

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
         },
     ];
 
-    const [searchTerm, setSearchTerm] = React.useState('');
+    const [searchTerm, setSearchTerm] = React.useState('React');
 
     const handleSearch = event => {
         setSearchTerm(event.target.value);
@@ -29,13 +29,13 @@ const App = () => {
     const searchedStories = stories.filter(story =>
         story.title
             .toLowerCase()
-            .includes(searchTerm));
+            .includes(searchTerm.toLowerCase()));
 
     return (
         <div>
             <h1>My Hacker Stories</h1>
 
-            <Search onSearch={handleSearch}/>
+            <Search search={searchTerm} onSearch={handleSearch}/>
 
             <hr/>
 
@@ -44,23 +44,29 @@ const App = () => {
     );
 };
 
+const List = ({list}) =>
+    list.map(({ objectID, ...item }) => <Item key={objectID} {...item} />);
 
-const List = props =>
-    props.list.map(item => (
-        <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-        </div>
-    ));
+const Item = ({ title, url, author, num_comments, points }) => (
+    <div>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+        <span>{author}</span>
+        <span>{num_comments}</span>
+        <span>{points}</span>
+    </div>
+);
 
-const Search = props => (
+const Search = ({search, onSearch}) => (
     <div>
         <label htmlFor="search">Search: </label>
-        <input id="search" type="text" onChange={props.onSearch}/>
+        <input
+            id="search"
+            type="text"
+            value={search}
+            onChange={onSearch}
+        />
     </div>
 );
 
